836b828a8470549b7ecd451fb6de391d59f06175
tanvir.kour <tanvir.kour@codenicely.in>
2024-03-17 08:15:18 +0530

    base router setup

---
+++ document_management_system/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from "../views/Login.vue"
+
+Vue.use(VueRouter)
+const routes = [
+    // before login
+    {
+        path: "/",
+        name: "login",
+        meta: { requiresAuth: false, title: "Login", show_back_btn: false },
+        component: Login,
+    },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+new Vue({
+    router
+})
+export default router;
